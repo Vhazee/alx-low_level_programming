5419828cf5207bb39cb830a9fe4532dd104b7a07
Vhazee <zwivhuyamuthathi@gmail.com>
2023-03-20 13:26:03 -0700

    Alphabets in lowercase using a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet lowercase
+ */
+void print_alphabet(void)
+{
+	char i;
+	/*For loop to loop through all the alphabets*/
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
